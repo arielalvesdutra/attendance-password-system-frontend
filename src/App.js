36e0636f0bf1dfe9ddbeb8b5bff70bb68ef53bdc
arielalvesdutra <alves.lsn@gmail.com>
2019-04-11 08:40:28 -0300

    Adiciona preenchimento, de exemplo, dos parametros do template Content

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ import Footer from './Components/Templates/Footer'
 import Header from './Components/Templates/Header'
 import Nav from './Components/Templates/Nav'
 
+import KeyLine from './Components/KeyLine'
+
 class App extends Component {
   render() {
     return (
       <div className="App d-flex flex-column align-items-center justify-content-between">
         <Header></Header>
         <Nav></Nav>
-        <Content></Content>
+        <Content title="Senhas emitidas">
+          <KeyLine />
+        </Content>
         <Footer></Footer>
       </div>
     );
