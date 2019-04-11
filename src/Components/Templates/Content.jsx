8dcf2f52860bc760c9f01ad342189b53fdba2283
arielalvesdutra <alves.lsn@gmail.com>
2019-04-11 08:37:53 -0300

    Adiciona preenchimento do parametro props.title e props.children no Content

--- src/Components/Templates/Content.jsx
+++ src/Components/Templates/Content.jsx
@@ -2,26 +2,14 @@ import React from 'react'
 
 import './Content.css'
 
-import KeyLine from '../KeyLine'
-
 export default props =>
     <div className="Content">
         <div className="Content-container col-12 col-md-10 col-sm-8 bg-light">
 
-        <h3>Senhas emitidas</h3>
-        <hr/>
-
-        <KeyLine />
-        <KeyLine />
-        <KeyLine />
-        <KeyLine />
-        <KeyLine />
-        <KeyLine />
-        <KeyLine />
-        <KeyLine />
-        <KeyLine />
-        <KeyLine />
-        <KeyLine /> 
-
+            <h3>
+                { props.title || 'Título do artigo' }
+            </h3>
+            <hr />
+            {props.children}
         </div>
     </div>
